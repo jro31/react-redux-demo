816aef575214c518db7bbf171ac00fe523ea7eb7
Jethro Williams <jethrowilliams@outlook.com>
2021-10-28 12:59:20 +0100

    Use the 'useSelector' react-redux hook to set-up a subscription to the Redux store, and to use the store state in a React component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,20 @@
+import { useSelector } from 'react-redux'; // There's also a 'useStore' hook
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  // You pass a function into 'useSelector' to retrieve the part of the state that you need in this component; in this case the 'counter'
+  // 'useSelector' will automatically set-up a subscription to the Redux store for this component
+  // So whenever the Redux store changes, this component will be re-executed
+  // This in turn means that the 'counter' (below) will always be the latest version of 'state.counter'
+  const counter = useSelector(state => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
