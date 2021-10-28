1f5610919efee1280ba2e4d4e4fd6b8c32efa307
Jethro Williams <jethrowilliams@outlook.com>
2021-10-28 12:30:54 +0100

    Create the Redux 'store' and 'provide' it to our React app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
+import store from './store/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// You don't have to wrap the '<App>' component with <Provider>; you can wrap lower-level components. But only components wrapped with <Provider> and their children, will have access to Redux
+// The <Provider> component takes a 'store' prop, and we're passing-in our store here
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root')
+);
